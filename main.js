5bc1702ed9651e8a4c33e745f915b0da3ce8dc32
Matt McKegg <matt@wetsand.co.nz>
2016-06-05 08:37:08 +1000

    update electron api calls

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var app = require('app')
-var BrowserWindow = require('browser-window')
+var app = require('electron').app
+var BrowserWindow = require('electron').BrowserWindow
 var mainWindow = null
 
 app.on('window-all-closed', function () {
